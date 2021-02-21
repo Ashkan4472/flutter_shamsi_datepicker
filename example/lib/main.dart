c8a2ef4d20d2316edd21c13aaa4526eb33f19a8e
Ashkan Tofangdar <ashkan4472@gmail.com>
2021-02-21 13:18:13 +0330

    init commit

---
+++ example/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:persian_datepicker/persian_datepicker.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Persian Datepicker test"),
+      ),
+      body: Center(
+        child: PersianDatepicker(),
+      ),
+    );
+  }
+}
---
+++ lib/persian_datepicker.dart
@@ -0,0 +1,17 @@
+library persian_datepicker;
+
+import 'package:flutter/material.dart';
+
+class PersianDatepicker extends StatefulWidget {
+  @override
+  _PersianDatepickerState createState() => _PersianDatepickerState();
+}
+
+class _PersianDatepickerState extends State<PersianDatepicker> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("persian date picker!"),
+    );
+  }
+}
